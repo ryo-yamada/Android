694361830cb0b73c30b771b355ff5100d52387e0
ryo-yamada <ryo.yamada.miwa@gmail.com>
2018-04-20 00:21:38 +0900

    課題1

--- Practice/SampleApp/app/src/main/java/sampleapp/practice/com/example/yuriyuri/sampleapp/data/repository/TagDataRepository.kt
+++ Practice/SampleApp/app/src/main/java/sampleapp/practice/com/example/yuriyuri/sampleapp/data/repository/TagDataRepository.kt
@@ -1,9 +1,10 @@
 package sampleapp.practice.com.example.yuriyuri.sampleapp.data.repository
 
 import io.reactivex.Flowable
+import sampleapp.practice.com.example.yuriyuri.model.TagId
 import sampleapp.practice.com.example.yuriyuri.model.TagModel
-import sampleapp.practice.com.example.yuriyuri.sampleapp.data.api.QiitaApi
 import sampleapp.practice.com.example.yuriyuri.sampleapp.data.api.NetworkClient
+import sampleapp.practice.com.example.yuriyuri.sampleapp.data.api.QiitaApi
 import sampleapp.practice.com.example.yuriyuri.sampleapp.util.AppSchedulerProvider
 import sampleapp.practice.com.example.yuriyuri.sampleapp.util.SchedulerProvider
 
@@ -29,5 +30,10 @@ class TagDataRepository : TagRepository {
         * TagModel:package sampleapp.practice.com.example.yuriyuri.model.TagModel
         * */
         // TODO:return new Flowable<List<TagModel>>
+        return qiitaApi.getTags(next, perPage, sortType).map { it ->
+            it.map { it ->
+                return@map TagModel(it.followersCount, it.tagIconImageUrl, TagId(it.tagId), it.itemsCount)
+            }
+        }.subscribeOn(appSchedulerProvider.io())
     }
 }
